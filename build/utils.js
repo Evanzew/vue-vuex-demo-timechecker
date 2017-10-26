f5812604865b54791f03d9b1f803cd0ad001b09b
Colin Miao <sddzok@163.com>
2017-10-26 15:47:23 +0800

    Made bootstrap font effctive

--- build/utils.js
+++ build/utils.js
@@ -38,12 +38,14 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
     }
   }
+  
 
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
