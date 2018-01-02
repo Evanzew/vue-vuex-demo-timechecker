27ad468cdabcd093268932a0b97d29054ce77862
Evan Zou <evan.zou@perficient.com>
2018-01-02 10:58:26 +0800

    file fixed

--- server/enum/ErrorEnum.js
+++ server/enum/ErrorEnum.js
@@ -9,9 +9,7 @@ var errorEnum = {
   SystemError: { code: '500', message: 'Internal Server Error' },
   ServiceUnavailable: { code: '503', message: 'Service Unavailable' },
   UnknownError: { code: '700', message: 'Unknown Error' },
-  NoEmployee: { code: '800', message: 'Sorry, there is no emmloyees' },
   NoUser: { code: '801', message: 'Sorry, user name or password is wrong' },
-  NotFoundById: { code: '802', message: 'Sorry, user can not find' },
   NoTaskList: { code: '803', message: 'Sorry, there is no task' },
   NoTaskFoundById: { code: '804', message: 'Sorry, task can not find' }
 };
